3808b2b9254231a118365a6df2be1ea8094e0247
zhulg <lg.json@gmail.com>
2022-10-31 00:09:51 +0800

    commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use clap::{Arg, ArgAction, Command};
+
+fn main() {
+    let matches = Command::new("Decompile APK")
+        .author("lg.json@gmail.com")
+        .version("1.0.0")
+        .about("Decompile APK for Android")
+        .arg(
+            Arg::new("file")
+                // .short('f')
+                .action(ArgAction::Set)
+                .index(1)
+                .default_value("-")
+                .help("The path to your apk."),
+        )
+        .after_help(
+            "Longer explanation to appear after the options when \
+                  displaying the help information from --help or -h",
+        )
+        .get_matches();
+
+    if let Some(c) = matches.get_one::<String>("file") {
+        println!("begin decompile file:{}", c);
+    }
+}
